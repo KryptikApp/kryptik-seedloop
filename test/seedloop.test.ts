7d28c50376c8fafaaec0a7885f0cba2f320b22f9
jettblu <jettblu.fabrications@gmail.com>
2022-03-27 23:25:44 -0400

    Add seedloop tests

---
+++ test/seedloop.test.ts
@@ -0,0 +1,29 @@
+import HDSeedLoop from "../src"
+import { defaultNetworks } from "../src/network.js";
+
+const validMnemonics = [
+    "square time hurdle gospel crash uncle flash tomorrow city space shine sad fence ski harsh salt need edit name fold corn chuckle resource else",
+    "until issue must",
+    "glass skin grass cat photo essay march detail remain",
+    "dream dinosaur poem cherry brief hand injury ice stuff steel bench vacant amazing bar uncover",
+    "mad such absent minor vapor edge tornado wrestle convince shy battle region adapt order finish foot follow monitor",
+  ]
+
+describe("SeedLoop", () => {
+    it("Keyrings can be constructed without a mnemonic", () => {
+        const seedLoop = new HDSeedLoop()
+        seedLoop.getKeyRing(defaultNetworks["btc"]).then((keyRing)=>{
+            expect(keyRing.id).toBeTruthy()
+            expect(keyRing.id.length).toBeGreaterThan(9)
+        });
+      })
+      it("can be constructed with a mnemonic", () => {
+        const seedLoop = new HDSeedLoop({
+          mnemonic: validMnemonics[0],
+        })
+        seedLoop.getKeyRing(defaultNetworks["btc"]).then((keyRing)=>{
+            expect(keyRing.id).toBeTruthy()
+            expect(keyRing.id.length).toBeGreaterThan(9)
+        });
+      })
+});
