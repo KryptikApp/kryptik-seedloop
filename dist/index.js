b1270ce6142ead52f5593900cb1958e0736f6985
jettblu <jettblu.fabrications@gmail.com>
2022-04-06 00:18:06 -0400

    Update packages

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./kryptik-seedloop.cjs.production.min.js')
+  module.exports = require('./hdseedloop.cjs.production.min.js')
 } else {
-  module.exports = require('./kryptik-seedloop.cjs.development.js')
+  module.exports = require('./hdseedloop.cjs.development.js')
 }
