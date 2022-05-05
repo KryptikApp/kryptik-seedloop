f532d6cc8127cd9207b5d3ae5705135836bc9bad
jettblu <jettblu.fabrications@gmail.com>
2022-05-04 22:34:25 -0400

    Remove bitcoin sig. support until further testing

--- src/walletKryptik.ts
+++ src/walletKryptik.ts
@@ -5,8 +5,8 @@ import { SigningKey } from "@ethersproject/signing-key";
 import { Keypair} from '@solana/web3.js'
 import * as bitcoin from 'bitcoinjs-lib'
 import * as nacl from "tweetnacl";
-import * as ecc from 'tiny-secp256k1';
-import { ECPairFactory,} from 'ecpair';
+// import * as ecc from 'tiny-secp256k1';
+// import { ECPairFactory,} from 'ecpair';
 
 
 
@@ -75,23 +75,25 @@ export class WalletKryptik extends WalletEthers{
 
     // signs btc family transaction
     async signBtcTransaction(btcTransaction:bitcoin.Psbt){
-        console.log(btcTransaction);
-        const privKeyBuffer:Buffer = Buffer.from(arrayify(this.privateKey));
-        const ECPair = ECPairFactory(ecc);
-        // create eckey from privkey buffer
-        let ecKey = ECPair.fromPrivateKey(privKeyBuffer);
-        // sign btc transaction
-        btcTransaction.signInput(0, ecKey);
-        // create validator for btc transaction
-        const validator = (
-            pubkey: Buffer,
-            msghash: Buffer,
-            signature: Buffer,
-          ): boolean => ECPair.fromPublicKey(pubkey).verify(msghash, signature);
-        // validate signature 
-        btcTransaction.validateSignaturesOfInput(0, validator);
-        return btcTransaction;
         throw Error("Bitcoin tx. signature not implemented yet.")
+        return btcTransaction;
+        // console.log(btcTransaction);
+        // const privKeyBuffer:Buffer = Buffer.from(arrayify(this.privateKey));
+        // const ECPair = ECPairFactory(ecc);
+        // // create eckey from privkey buffer
+        // let ecKey = ECPair.fromPrivateKey(privKeyBuffer);
+        // // sign btc transaction
+        // btcTransaction.signInput(0, ecKey);
+        // // create validator for btc transaction
+        // const validator = (
+        //     pubkey: Buffer,
+        //     msghash: Buffer,
+        //     signature: Buffer,
+        //   ): boolean => ECPair.fromPublicKey(pubkey).verify(msghash, signature);
+        // // validate signature 
+        // btcTransaction.validateSignaturesOfInput(0, validator);
+        // return btcTransaction;
+        
     }
 
     // can sign data OR transaction!
