5589339c83ea7a7252bce3989e76c5c4033792c8
jettblu <jettblu.fabrications@gmail.com>
2022-06-26 21:23:40 -0700

    Delete seedloop bitcoin test

--- test/seedloop.test.ts
+++ test/seedloop.test.ts
@@ -1,5 +1,5 @@
 import HDSeedLoop from "../src"
-import { defaultNetworks } from "../src/network";
+import { defaultNetworks} from "../src/network";
 
 const validMnemonics = [
   "square time hurdle gospel crash uncle flash tomorrow city space shine sad fence ski harsh salt need edit name fold corn chuckle resource else",
@@ -109,6 +109,45 @@ describe("SeedLoop", () => {
         for (let ticker in defaultNetworks) {
           expect(seedLoop.networkOnSeedloop(defaultNetworks[ticker])).toBeTruthy();
         }
-        
       })
+      // it("signs bitcoin transaction correctly", async () => {
+      //   const seedLoop = new HDSeedLoop();
+      //   const psbt = new bitcoin.Psbt();
+      //   psbt.setVersion(2); // These are defaults. This line is not needed.
+      //   psbt.setLocktime(0); // These are defaults. This line is not needed.
+      //   psbt.addInput({
+      //     // if hash is string, txid, if hash is Buffer, is reversed compared to txid
+      //     hash: '7d067b4a697a09d2c3cff7d4d9506c9955e93bff41bf82d439da7d030382bc3e',
+      //     index: 0,
+      //     sequence: 0xffffffff, // These are defaults. This line is not needed.
+    
+      //     // non-segwit inputs now require passing the whole previous tx as Buffer
+      //     nonWitnessUtxo: Buffer.from(
+      //       '0200000001f9f34e95b9d5c8abcd20fc5bd4a825d1517be62f0f775e5f36da944d9' +
+      //         '452e550000000006b483045022100c86e9a111afc90f64b4904bd609e9eaed80d48' +
+      //         'ca17c162b1aca0a788ac3526f002207bb79b60d4fc6526329bf18a77135dc566020' +
+      //         '9e761da46e1c2f1152ec013215801210211755115eabf846720f5cb18f248666fec' +
+      //         '631e5e1e66009ce3710ceea5b1ad13ffffffff01' +
+      //         // value in satoshis (Int64LE) = 0x015f90 = 90000
+      //         '905f010000000000' +
+      //         // scriptPubkey length
+      //         '19' +
+      //         // scriptPubkey
+      //         '76a9148bbc95d2709c71607c60ee3f097c1217482f518d88ac' +
+      //         // locktime
+      //         '00000000',
+      //       'hex',
+      //     ),
+      //   });
+      //   psbt.addOutput({
+      //     address: '1KRMKfeZcmosxALVYESdPNez1AP1mEtywp',
+      //     value: 80000,
+      //   });
+      //   // btc network
+      //   let btcNetwork = defaultNetworks.btc;
+      //   // btc address
+      //   let btcAddysAll:string[] = await seedLoop.getAddresses(btcNetwork);
+      //   let btcAddy:string = btcAddysAll[0];
+      //   expect(await seedLoop.signTransaction(btcAddy, {btcTransaction:psbt}, defaultNetworks.btc)).toHaveReturned();
+      // })
 });
