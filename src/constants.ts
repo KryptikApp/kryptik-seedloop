787d45312dee9df964a71b2575216f356a8dda9b
jettblu <jettblu.fabrications@gmail.com>
2022-06-26 21:19:56 -0700

    Add dictionary key constants for network families

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const EVM_FAMILY_KEYRING_NAME:string = "evmfamily"
+export const NEAR_FAMILY_KEYRING_NAME:string = "nearfamily"
+export const SOLANA_FAMILY_KEYRING_NAME:string = "solanafamily"
